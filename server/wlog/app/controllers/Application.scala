d2b533beaab637791dee24c3a1ea6a83a98c4bb7
T.N <i_takanaga@mac.com>
2013-05-03 20:25:53 +0900

    リクエストされた値を反転してJSONで返す処理追加

--- server/wlog/app/controllers/Application.scala
+++ server/wlog/app/controllers/Application.scala
@@ -8,5 +8,13 @@ object Application extends Controller {
   def index = Action {
     Ok(views.html.index("Your new application is ready."))
   }
+
+  def reverse(value: String) = Action {
+      import play.api.libs.json.Json
   
+      val success = Map("value1" -> value.reverse)
+      val json = Json.toJson(success)
+  
+      Ok(json).as(JSON)
+  }
 }
